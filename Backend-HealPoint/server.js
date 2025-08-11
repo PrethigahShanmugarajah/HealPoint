89e30f9bc86da51f791171e5521fbe6e99b4260b
PrethigahShanmugarajah <prethigah@gmail.com>
2025-08-11 16:28:09 +0530

    Created Backend folder with initial folder structure

---
+++ Backend-HealPoint/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+/*-------- App Config --------*/
+const app = express()
+const port = process.env.PORT || 4000
+
+/*-------- Middlewares --------*/
+app.use(express.json())
+app.use(cors())
+
+/*-------- API Endpoint --------*/
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port, () => {
+    console.log("Server Started", port)
+})
