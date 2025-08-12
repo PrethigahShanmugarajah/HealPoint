bf83d1e1d58340a5572d3f205d97862b3c974c11
PrethigahShanmugarajah <prethigah@gmail.com>
2025-08-12 23:53:07 +0530

    Created Admin folder and added initial files

--- Admin-HealPoint/vite.config.js
+++ Admin-HealPoint/vite.config.js
@@ -4,13 +4,14 @@
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
+//   server: { port: 5174 }
 // })
 
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server:{port:5173}
-});
+  plugins: [
+    tailwindcss(),
+  ],
+  server: { port: 5174 }
+})
